790042163d446807d68381f579ee10ff53a46dbd
Oleguer <oleguercongost@gmail.com>
2023-12-21 11:19:08 +0100

    Added return to ContractSetOwnerProcessor

--- src/State/ContractSetOwnerProcessor.php
+++ src/State/ContractSetOwnerProcessor.php
@@ -11,18 +11,16 @@ use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
 #[AsDecorator('api_platform.doctrine.orm.state.persist_processor')]
 class ContractSetOwnerProcessor implements ProcessorInterface
 {
-
-
     /**
      * ContractSetOwnerProcessor constructor.
      */
     public function __construct(private ProcessorInterface $processor, private Security $security){}
 
-    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
     {
         if($data instanceof Contract && $data->getOwner() === null && $this->security->getUser()){
             $data->setOwner($this->security->getUser());
         }
-        $this->processor->process($data, $operation, $uriVariables, $context);
+        return $this->processor->process($data, $operation, $uriVariables, $context);
     }
 }
