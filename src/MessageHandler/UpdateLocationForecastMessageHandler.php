e811048a5f3cba2ba4f2c4342254b8855d9b062d
Oleguer <oleguercongost@gmail.com>
2024-01-08 11:01:26 +0100

    Implemented UpdateLocationForecastMessageHandler

--- src/MessageHandler/UpdateLocationForecastMessageHandler.php
+++ src/MessageHandler/UpdateLocationForecastMessageHandler.php
@@ -4,6 +4,8 @@
 namespace App\MessageHandler;
 
 use App\Message\UpdateLocationForecastMessage;
+use App\Service\ForecastAPIConnection\ForecastAPIConnectionInterface;
+use App\Service\ForecastUpdater;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
@@ -11,13 +13,19 @@ use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 class UpdateLocationForecastMessageHandler
 {
 
-    public function __construct(private LoggerInterface $logger)
-    {
+    public function __construct(
+        private LoggerInterface $logger,
+        private ForecastAPIConnectionInterface $forecastAPIConnection,
+        private ForecastUpdater $forecastUpdater
+    ){
+
     }
 
     public function __invoke(UpdateLocationForecastMessage $message)
     {
-        sleep(5);
-        $this->logger->info($message->getLocation());
+        $forecastsDTOs = $this->forecastAPIConnection->getForecastsDTOs($message->getLocation());
+        foreach ($forecastsDTOs as $forecastsDTO){
+            $this->forecastUpdater->update($forecastsDTO);
+        }
     }
 }
